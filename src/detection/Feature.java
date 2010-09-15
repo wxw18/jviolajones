22e2af602dba8cd19399acb8921f8b8c055919a9
saimonfon <saimonfon@gmail.com>
2010-09-15 21:12:14 +0000

    Size of the detector is now read in XML (and no more hardcoded)

--- src/detection/Feature.java
+++ src/detection/Feature.java
@@ -3,6 +3,7 @@ package detection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Point;
 
 public class Feature {
 
@@ -11,22 +12,25 @@ public class Feature {
 	float threshold;
 	float left_val;
 	float right_val;
-	public Feature(	float threshold,float left_val,	float right_val) {
+	Point size;
+	public Feature(	float threshold,float left_val,	float right_val,Point size) {
 		nb_rects = 0;
 		rects=new Rect[3];
 		this.threshold=threshold;
 		this.left_val=left_val;
 		this.right_val=right_val;
+		this.size = size;
 	}
 
 	public float getVal(int[][] grayImage, int[][] squares, int i, int j, float scale) {
-		int w=(int) (scale*24);
-		double w2=1./(w*w);
+		int w=(int) (scale*size.x);
+		int h=(int)(scale*size.y);
+		double inv_area=1./(w*h);
 		//System.out.println("w2 : "+w2);
-		int total_x=grayImage[i+w][j+w]+grayImage[i][j]-grayImage[i][j+w]-grayImage[i+w][j];
-		int total_x2=squares[i+w][j+w]+squares[i][j]-squares[i][j+w]-squares[i+w][j];
-		double moy=total_x*w2;
-		double vnorm=total_x2*w2-moy*moy;
+		int total_x=grayImage[i+w][j+h]+grayImage[i][j]-grayImage[i][j+h]-grayImage[i+w][j];
+		int total_x2=squares[i+w][j+h]+squares[i][j]-squares[i][j+h]-squares[i+w][j];
+		double moy=total_x*inv_area;
+		double vnorm=total_x2*inv_area-moy*moy;
 		vnorm=(vnorm>1)?Math.sqrt(vnorm):1;
 
 				int rect_sum=0;
@@ -41,7 +45,7 @@ public class Feature {
 					rect_sum+=(int)((grayImage[rx2][ry2]-grayImage[rx1][ry2]-grayImage[rx2][ry1]+grayImage[rx1][ry1])*r.weight);
 				}
 				//System.out.println(rect_sum);
-				double rect_sum2=rect_sum*w2;
+				double rect_sum2=rect_sum*inv_area;
 
 		return (rect_sum2<threshold*vnorm?left_val:right_val);
 	}
